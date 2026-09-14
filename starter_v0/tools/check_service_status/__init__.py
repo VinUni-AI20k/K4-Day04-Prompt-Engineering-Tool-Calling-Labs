9320dbcaf064e988b6cd450eb7f853d52482d09e
Khoi Pham <kpham6488@gmail.com>
2026-09-14 18:48:16 +0700

    Init file tool

--- starter_v0/tools/check_service_status/__init__.py
+++ starter_v0/tools/check_service_status/__init__.py
@@ -1,1 +1,1 @@
-
+"""Check service status tool package.\n\nThis folder is a Python package used to organize the tool implementation.\n"""
--- starter_v0/tools/inspect_device/__init__.py
+++ starter_v0/tools/inspect_device/__init__.py
@@ -1,1 +1,1 @@
-
+"""Tool package marker.\n\nThis folder is a Python package for organizing a single helpdesk tool implementation.\n"""
